6e05852d52db96d098d45843c18ec79f66ce5f4e
wspandihai <2711002357@qq.com>
2015-09-27 09:22:09 +0800

    新增

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,46 @@
 module.exports = function(grunt) {
 
+
+
+  // LiveReload的默认端口号，你也可以改成你想要的端口号
+  var lrPort = 35729;
+  // 使用connect-livereload模块，生成一个与LiveReload脚本
+  // <script src="http://127.0.0.1:35729/livereload.js?snipver=1" type="text/javascript"></script>
+  var lrSnippet = require('connect-livereload')({
+    port: lrPort
+  });
+  // 使用 middleware(中间件)，就必须关闭 LiveReload 的浏览器插件
+  var lrMiddleware = function(connect, options) {
+    return [
+      // 把脚本，注入到静态文件中
+      lrSnippet,
+      // 静态文件服务器的路径
+      connect.static(options.base[0]),
+      // 启用目录浏览(相当于IIS中的目录浏览)
+      connect.directory(options.base[0])
+    ];
+  }
+
+
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      options: {
+        // 服务器端口号
+        port: 8180,
+        // 服务器地址(可以使用主机名localhost，也能使用IP)
+        hostname: 'localhost',
+        // 物理路径(默认为. 即根目录) 使用'.'或'..'为路径的时，可能会返回403 Forbidden. 此时将该值改为相对路径 。
+        base: '.'
+      },
+      livereload: {
+        options: {
+          // 通过LiveReload脚本，让页面重新加载。
+          middleware: lrMiddleware
+        }
+      }
+    },
     /* 压缩优化图片大小 */
     imagemin: {
       dist: {
@@ -13,7 +51,7 @@ module.exports = function(grunt) {
           expand: true,
           cwd: 'images/original',
           src: ['**/*.{png,jpg,jpeg}'], // 优化 img 目录下所有 png/jpg/jpeg 图片
-         dest: 'images/production' // 优化后的图片保存位置，默认覆盖
+          dest: 'images/production' // 优化后的图片保存位置，默认覆盖
         }]
       }
     },
@@ -22,6 +60,7 @@ module.exports = function(grunt) {
       sprity: {}
     },
     watch: {
+
       /* 监控icon合并图片,生成less */
       icon: {
         files: ['images/icon/**/*.{png,jpg,jpeg}'],
@@ -41,6 +80,7 @@ module.exports = function(grunt) {
       //less动态编译
       less: {
         files: ['less/**/*.less'],
+        event: ['changed', 'added'],
         options: {
           livereload: false
         },
@@ -56,7 +96,7 @@ module.exports = function(grunt) {
       //监控js文件运行jshint任务
       js: {
         options: {
-          livereload: true
+          spawn: false
         },
         files: ['js/**/*.js'],
         tasks: ['jshint']
@@ -66,11 +106,13 @@ module.exports = function(grunt) {
         options: {
           livereload: true
         },
-        files: ['*.html']
+        files: ['html/*.html']
       }
     },
     jshint: {
-      all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
+      all: {
+        src: ''
+      }
     },
     less: {
       compileCore: {
@@ -80,17 +122,27 @@ module.exports = function(grunt) {
         },
         expand: true,
         cwd: './less/',
-        src: ['production/*.less'],
+        src: ['production/**/*.less'],
         dest: './css/',
         ext: '.css'
       },
     }
+  });
+
+
+  grunt.event.on('watch', function(action, filepath) {
+    grunt.config('jshint.all.src', filepath);
   });
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-eshsprity');
-  grunt.registerTask('default', ['watch']);
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+  grunt.registerTask('all', ['eshsprity', 'imagemin']);
+  grunt.registerTask('default', ['connect', 'watch']);
+  // grunt.registerTask('default', ['watch']);
 
 };
